b55415b9899124e02e75475db98d77d7b357a9f9
Agustin Marengo <agustinmrgo@gmail.com>
2022-07-28 16:08:45 -0300

    dayMenuData array descruturing

--- food-app/src/pages/MenuPageAccessible.js
+++ food-app/src/pages/MenuPageAccessible.js
@@ -5,16 +5,19 @@ export const MenuPageAccessible = () => {
     <section className="container px-4 mx-auto">
       <h1>Menu</h1>
       <div>
-        {Object.entries(menuData).map((dayMenuData) => (
-          <div key={dayMenuData[0]}>
-            <h2>{dayMenuData[0]}</h2>
-            <ul>
-              {dayMenuData[1].map((plate) => (
-                <li key={plate.id}>{plate.nombre}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
+        {Object.entries(menuData).map((dayMenuData) => {
+          const [day, dayMenu] = dayMenuData;
+          return (
+            <div key={day}>
+              <h2>{day}</h2>
+              <ul>
+                {dayMenu.map((plate) => (
+                  <li key={plate.id}>{plate.nombre}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
       </div>
 
       {/* TODO: add clickable tiles that open a modal to see more about recommended plates but use div with on click so keyboard users can't access the modal   */}
