bbaf031977a9ba2f31f15c8dff14c445d3d4a2f1
Alan Dours <alandours93@gmail.com>
2022-08-22 14:36:32 -0300

    Add skip to main content link

---
+++ food-app/src/components/SkipToMainContent.js
@@ -0,0 +1,8 @@
+export const SkipToMainContent = () => (
+  <a
+    href="#main"
+    className="text-black absolute focus:left-0 focus:top-0 bg-orange-400 p-5 -left-full -top-full hover:text-gray-800 active:text-gray-800"
+  >
+    Ir al contenido principal
+  </a>
+);
