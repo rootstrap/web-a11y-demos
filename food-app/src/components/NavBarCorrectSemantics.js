66bd1854945d108d889b59648efac4cd0cdba425
Alan Dours <alandours93@gmail.com>
2022-08-23 12:55:40 -0300

    Add link to second navbar

--- food-app/src/components/NavBarCorrectSemantics.js
+++ food-app/src/components/NavBarCorrectSemantics.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { routes } from "../utils/routes";
+import { SkipToMainContent } from './SkipToMainContent';
 
 export const NavBar = () => {
   return (
@@ -7,6 +8,7 @@ export const NavBar = () => {
       <div className="container flex items-center px-4 mx-auto my-4">
         <img src="/logo.png" className="mr-16" />
         <div className="flex space-x-16 text-2xl font-extrabold text-black hover:text-gray-800 active:text-gray-800">
+          <SkipToMainContent />
           <Link
             to={routes.HOME}
             className="text-black hover:text-gray-800 active:text-gray-800"
