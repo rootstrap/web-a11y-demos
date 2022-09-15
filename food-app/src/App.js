ada2c25c8704b16012ee335580e09d6fb86c4717
Alan Dours <alan.dours@rootstrap.com>
2022-09-15 16:37:25 -0300

    Fix img src

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -16,7 +16,7 @@ import { routes } from "./utils/routes";
 function App() {
   return (
     <Router>
-      <img src="circle_1.png" className="absolute top-0 right-0 w-1/2 -z-50" />
+      <img src="/circle_1.png" className="absolute top-0 right-0 w-1/2 -z-50" />
 
       <NavBar />
       <main id="main">
