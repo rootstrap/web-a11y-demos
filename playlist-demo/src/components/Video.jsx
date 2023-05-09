d95bf58f8e3542c4d287c63fc05ce4034042a638
Alan Dours <alan.dours@rootstrap.com>
2023-05-09 16:24:29 -0300

    Keep only food-app

--- playlist-demo/src/components/Video.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import cn from "classnames";
-
-const Video = ({ className }) => (
-  <div className={cn("bg-gray-300 aspect-video", className)} />
-);
-
-export default Video;
--- playlist-demo/src/helpers.js
+++
@@ -1,5 +0,0 @@
-export const isWatched = (progress) => progress > 90;
-
-export const trackClick = (location) => {
-  console.log("🚀 new Click by the user - Location:", location);
-};
