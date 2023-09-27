be2eaa648a6b2a64e6abd4e95579c5c404c9bd19
Agustina Chaer <aguschaer@gmail.com>
2023-09-27 19:01:13 -0300

    switch mode button and fixes

--- food-app/src/pages/AboutPage.js
+++ food-app/src/pages/AboutPage.js
@@ -2,38 +2,55 @@ export const AboutPage = () => {
   return (
     <div className="container px-4 mx-auto">
       <div className="h1">¿Quiénes Somos?</div>
-      {/* TODO: Add video without captions */}
       <div className="p">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beatus sibi
-        videtur esse moriens. <i>Sed haec omittamus;</i>{" "}
-        <b>Tum Torquatus: Prorsus, inquit, assentior;</b> Igitur ne dolorem
+        videtur esse moriens. Sed haec omittamus;{" "}
+        Tum Torquatus: Prorsus, inquit, assentior; Igitur ne dolorem
         quidem. Quid in isto egregio tuo officio et tanta fide-sic enim
         existimo-ad corpus refers? Nam de isto magna dissensio est. Atqui
         reperies, inquit, in hoc quidem pertinacem; Duo Reges: constructio
         interrete. Quis istud possit, inquit, negare? Quod non faceret, si in
         voluptate summum bonum poneret.{" "}
       </div>
-
-      <div className="ol">
-        <div className="li">
-          Ergo, si semel tristior effectus est, hilara vita amissa est?
-        </div>
-        <div className="li">
-          Non quaero, quid dicat, sed quid convenienter possit rationi et
-          sententiae suae dicere.
-        </div>
-        <div className="li">Quis istud possit, inquit, negare?</div>
-        <div className="li">
-          Quem si tenueris, non modo meum Ciceronem, sed etiam me ipsum abducas
-          divcebit.
-        </div>
+      <div className="p">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beatus sibi
+        videtur esse moriens. Sed haec omittamus;{" "}
+        Tum Torquatus: Prorsus, inquit, assentior; Igitur ne dolorem
+        quidem. Quid in isto egregio tuo officio et tanta fide-sic enim
+        existimo-ad corpus refers? Nam de isto magna dissensio est. Atqui
+        reperies, inquit, in hoc quidem pertinacem; Duo Reges: constructio
+        interrete. Quis istud possit, inquit, negare? Quod non faceret, si in
+        voluptate summum bonum poneret.{" "}
       </div>
-
-      <div className="ul">
-        <div className="li">Summus dolor plures dies manere non potest?</div>
-        <div className="li">
-          Sed quid attinet de rebus tam apertis plura requirere?
-        </div>
+      <div className="p">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beatus sibi
+        videtur esse moriens. Sed haec omittamus;{" "}
+        Tum Torquatus: Prorsus, inquit, assentior; Igitur ne dolorem
+        quidem. Quid in isto egregio tuo officio et tanta fide-sic enim
+        existimo-ad corpus refers? Nam de isto magna dissensio est. Atqui
+        reperies, inquit, in hoc quidem pertinacem; Duo Reges: constructio
+        interrete. Quis istud possit, inquit, negare? Quod non faceret, si in
+        voluptate summum bonum poneret.{" "}
+      </div>
+      <div className="p">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beatus sibi
+        videtur esse moriens. Sed haec omittamus;{" "}
+        Tum Torquatus: Prorsus, inquit, assentior; Igitur ne dolorem
+        quidem. Quid in isto egregio tuo officio et tanta fide-sic enim
+        existimo-ad corpus refers? Nam de isto magna dissensio est. Atqui
+        reperies, inquit, in hoc quidem pertinacem; Duo Reges: constructio
+        interrete. Quis istud possit, inquit, negare? Quod non faceret, si in
+        voluptate summum bonum poneret.{" "}
+      </div>
+      <div className="p">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beatus sibi
+        videtur esse moriens. Sed haec omittamus;{" "}
+        Tum Torquatus: Prorsus, inquit, assentior; Igitur ne dolorem
+        quidem. Quid in isto egregio tuo officio et tanta fide-sic enim
+        existimo-ad corpus refers? Nam de isto magna dissensio est. Atqui
+        reperies, inquit, in hoc quidem pertinacem; Duo Reges: constructio
+        interrete. Quis istud possit, inquit, negare? Quod non faceret, si in
+        voluptate summum bonum poneret.{" "}
       </div>
 
       <div className="h2">Historia</div>
@@ -45,14 +62,12 @@ export const AboutPage = () => {
         vitia nihil interesse. Quid, si etiam iucunda memoria est praeteritorum
         malorum?{" "}
       </div>
-
       <div className="p">
         Non est enim vitium in oratione solum, sed etiam in moribus. Qua ex
         cognitione facilior facta est investigatio rerum occultissimarum.
         Videamus animi partes, quarum est conspectus illustrior; Memini vero,
         inquam;{" "}
       </div>
-
       <div className="p">
         Quod autem principium officii quaerunt, melius quam Pyrrho; Aliter enim
         nosmet ipsos nosse non possumus. Verum tamen cum de rebus grandioribus
@@ -60,7 +75,46 @@ export const AboutPage = () => {
         diligi et carum esse iucundum est propterea, quia tutiorem vitam et
         voluptatem pleniorem efficit.{" "}
       </div>
-
+      <div className="p">
+        Aufidio, praetorio, erudito homine, oculis capto, saepe audiebam, cum se
+        lucis magis quam utilitatis desiderio moveri diceret. Rationis enim
+        perfectio est virtus; Et hunc idem dico, inquieta sed ad virtutes et ad
+        vitia nihil interesse. Quid, si etiam iucunda memoria est praeteritorum
+        malorum?{" "}
+      </div>
+      <div className="p">
+        Non est enim vitium in oratione solum, sed etiam in moribus. Qua ex
+        cognitione facilior facta est investigatio rerum occultissimarum.
+        Videamus animi partes, quarum est conspectus illustrior; Memini vero,
+        inquam;{" "}
+      </div>
+      <div className="p">
+        Quod autem principium officii quaerunt, melius quam Pyrrho; Aliter enim
+        nosmet ipsos nosse non possumus. Verum tamen cum de rebus grandioribus
+        dicas, ipsae res verba rapiunt; <i>Primum quid tu dicis breve?</i> Nam
+        diligi et carum esse iucundum est propterea, quia tutiorem vitam et
+        voluptatem pleniorem efficit.{" "}
+      </div>
+      <div className="p">
+        Aufidio, praetorio, erudito homine, oculis capto, saepe audiebam, cum se
+        lucis magis quam utilitatis desiderio moveri diceret. Rationis enim
+        perfectio est virtus; Et hunc idem dico, inquieta sed ad virtutes et ad
+        vitia nihil interesse. Quid, si etiam iucunda memoria est praeteritorum
+        malorum?{" "}
+      </div>
+      <div className="p">
+        Non est enim vitium in oratione solum, sed etiam in moribus. Qua ex
+        cognitione facilior facta est investigatio rerum occultissimarum.
+        Videamus animi partes, quarum est conspectus illustrior; Memini vero,
+        inquam;{" "}
+      </div>
+      <div className="p">
+        Quod autem principium officii quaerunt, melius quam Pyrrho; Aliter enim
+        nosmet ipsos nosse non possumus. Verum tamen cum de rebus grandioribus
+        dicas, ipsae res verba rapiunt; <i>Primum quid tu dicis breve?</i> Nam
+        diligi et carum esse iucundum est propterea, quia tutiorem vitam et
+        voluptatem pleniorem efficit.{" "}
+      </div>
       <div className="p">
         Et certamen honestum et disputatio splendida! omnis est enim de virtutis
         dignitate contentio. Sed mehercule pergrata mihi oratio tua. Ille
@@ -77,6 +131,66 @@ export const AboutPage = () => {
         vitium consuetudo Latina traduceret. Itaque ad tempus ad Pisonem omnes.
         Nec enim, omnes avaritias si aeque avaritias esse dixerimus, sequetur ut
         etiam aequas esse dicamus. Prioris generis est docilitas, memoria;{" "}
+      </div>
+      <div className="p">
+        Non est enim vitium in oratione solum, sed etiam in moribus. Qua ex
+        cognitione facilior facta est investigatio rerum occultissimarum.
+        Videamus animi partes, quarum est conspectus illustrior; Memini vero,
+        inquam;{" "}
+      </div>
+      <div className="p">
+        Quod autem principium officii quaerunt, melius quam Pyrrho; Aliter enim
+        nosmet ipsos nosse non possumus. Verum tamen cum de rebus grandioribus
+        dicas, ipsae res verba rapiunt; <i>Primum quid tu dicis breve?</i> Nam
+        diligi et carum esse iucundum est propterea, quia tutiorem vitam et
+        voluptatem pleniorem efficit.{" "}
+      </div>
+      <div className="p">
+        Et certamen honestum et disputatio splendida! omnis est enim de virtutis
+        dignitate contentio. Sed mehercule pergrata mihi oratio tua. Ille
+        incendat? Duo enim genera quae erant, fecit tria. Non igitur de improbo,
+        sed de callido improbo quaerimus, qualis Q. An ea, quae per vinitorem
+        antea consequebatur, per se ipsa curabit?{" "}
+      </div>
+      <div className="p">
+        Non est enim vitium in oratione solum, sed etiam in moribus. Qua ex
+        cognitione facilior facta est investigatio rerum occultissimarum.
+        Videamus animi partes, quarum est conspectus illustrior; Memini vero,
+        inquam;{" "}
+      </div>
+      <div className="p">
+        Quod autem principium officii quaerunt, melius quam Pyrrho; Aliter enim
+        nosmet ipsos nosse non possumus. Verum tamen cum de rebus grandioribus
+        dicas, ipsae res verba rapiunt; <i>Primum quid tu dicis breve?</i> Nam
+        diligi et carum esse iucundum est propterea, quia tutiorem vitam et
+        voluptatem pleniorem efficit.{" "}
+      </div>
+      <div className="p">
+        Et certamen honestum et disputatio splendida! omnis est enim de virtutis
+        dignitate contentio. Sed mehercule pergrata mihi oratio tua. Ille
+        incendat? Duo enim genera quae erant, fecit tria. Non igitur de improbo,
+        sed de callido improbo quaerimus, qualis Q. An ea, quae per vinitorem
+        antea consequebatur, per se ipsa curabit?{" "}
+      </div>
+      <div className="p">
+        Non est enim vitium in oratione solum, sed etiam in moribus. Qua ex
+        cognitione facilior facta est investigatio rerum occultissimarum.
+        Videamus animi partes, quarum est conspectus illustrior; Memini vero,
+        inquam;{" "}
+      </div>
+      <div className="p">
+        Quod autem principium officii quaerunt, melius quam Pyrrho; Aliter enim
+        nosmet ipsos nosse non possumus. Verum tamen cum de rebus grandioribus
+        dicas, ipsae res verba rapiunt; <i>Primum quid tu dicis breve?</i> Nam
+        diligi et carum esse iucundum est propterea, quia tutiorem vitam et
+        voluptatem pleniorem efficit.{" "}
+      </div>
+      <div className="p">
+        Et certamen honestum et disputatio splendida! omnis est enim de virtutis
+        dignitate contentio. Sed mehercule pergrata mihi oratio tua. Ille
+        incendat? Duo enim genera quae erant, fecit tria. Non igitur de improbo,
+        sed de callido improbo quaerimus, qualis Q. An ea, quae per vinitorem
+        antea consequebatur, per se ipsa curabit?{" "}
       </div>
     </div>
   );
